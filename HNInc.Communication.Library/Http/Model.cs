481b2b1ef7c88d76dcfd7bf56bd6cffd05fec3ed
ki93 <hogen123@naver.com>
2022-01-21 09:45:20 +0900

    Add Library Folder

---
+++ HNInc.Communication.Library/Http/Model.cs
@@ -0,0 +1,311 @@
+﻿using HNInc.Communication.Library.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+namespace HNInc.Communication.Library
+{
+    public class HttpDeiviceHealthCheck
+    {
+        #region 속성
+        [JsonPropertyName("status")]
+        public string _status { get; set; }
+        public string _requestResult { get; set; }
+        #endregion
+        #region ToString 재정의
+        public override string ToString() => $"Status : {_status}, RequestResult : {_requestResult}";
+        #endregion
+    }
+    public class HttpProductCounts
+    {
+        #region 속성
+        public string _date { get; set; }
+        public int _count { get; set; }
+        public string _requestResult { get; set; }
+        #endregion
+        #region 생성자 
+        public HttpProductCounts(string date, int count, string requestResult)
+        {
+            _date = date;
+            _count = count;
+            _requestResult = requestResult;
+        }
+        #endregion
+        #region ToString 재정의
+        public override string ToString() => $"Date : {_date}, Count : {_count}, RequestResult : {_requestResult}";
+        #endregion
+    }
+    public class HttpCycleTime
+    {
+        #region 속성
+        [JsonPropertyName("time")]
+        public string _time { get; set; }
+        [JsonPropertyName("cycleTime")]
+        public int _cycleTime { get; set; }
+        [JsonPropertyName("endTime")]
+        public string _endTime { get; set; }
+        [JsonPropertyName("startTime")]
+        public string _startTime { get; set; }
+        [JsonPropertyName("transCycleTime")]
+        public string _transCycleTime { get; set; }
+        public string _requestResult { get; set; }
+        #endregion
+        #region 생성자 
+        public HttpCycleTime(){}
+        public HttpCycleTime(string time, int cycleTime, string endTime, string startTime, string transCycleTime, string requestResult)
+        {
+            _time = time;
+            _cycleTime = cycleTime;
+            _endTime = endTime;
+            _startTime = startTime;
+            _transCycleTime = transCycleTime;
+            _requestResult = requestResult;
+        }
+        #endregion
+        #region ToString 재정의
+        public override string ToString() => $"Time : {_time}, CycleTime : {_cycleTime}, EndTime : {_endTime}, StartTime : {_startTime}, TransCycleTime : {_transCycleTime}, RequestResult : {_requestResult}";
+        #endregion
+    }
+    public class HttpCycleTimeAverage
+    {
+        #region 속성
+        [JsonPropertyName("mean")]
+        public double _mean { get; set; }
+        public string _requestResult { get; set; }
+        #endregion
+        #region 생성자 
+        public HttpCycleTimeAverage() { }
+        public HttpCycleTimeAverage(double mean, string requestResult)
+        {
+            _mean = mean;
+            _requestResult = requestResult;
+        }
+        #endregion
+        #region ToString 재정의
+        public override string ToString() => $"Status : {_mean}, RequestResult : {_requestResult}";
+        #endregion
+    }
+    public class HttpCycleInformaiton
+    {
+        #region 속성
+        public string _time { get; set; }
+        public string _scalePredict { get; set; }
+        public string _scaleLoad { get; set; }
+        public string _mae { get; set; }
+        public string _requestResult { get; set; }
+        #endregion
+        #region 생성자 
+        public HttpCycleInformaiton(string time, string scalePredict, string scaleLoad, string mae, string requestResult)
+        {
+            _time = time;
+            _scalePredict = scalePredict;
+            _scaleLoad = scaleLoad;
+            _mae = mae;
+            _requestResult = requestResult;
+        }
+        #endregion
+        #region ToString 재정의
+        public override string ToString() => $"Time : {_time}, ScalePredict : {_scalePredict}, ScaleLoad : {_scaleLoad}, Mae : {_mae}, RequestResult : {_requestResult}";
+        #endregion
+    }
+    public class HttpRealTimeCount
+    {
+        #region 속성
+        [JsonPropertyName("counts")]
+        public int _count { get; set; }
+        public string _requestResult { get; set; }
+        #endregion
+        #region 생성자 
+        public HttpRealTimeCount() { }
+        public HttpRealTimeCount(int count, string requestResult)
+        {
+            _count = count;
+            _requestResult = requestResult;
+        }
+        #endregion
+        #region ToString 재정의
+        public override string ToString() => $"Counts : {_count}, RequestResult : {_requestResult}";
+        #endregion
+    }
+    public class HttpSpindleLoad
+    {
+        #region 속성
+        public string _time { get; set; }
+        public string _meanScalePredict { get; set; }
+        public string _meanScaleLoad { get; set; }
+        public string _meanMae { get; set; }
+        public string _requestResult { get; set; }
+        #endregion
+        #region 생성자 
+        public HttpSpindleLoad(string time, string meanScalePredict, string meanScaleLoad, string meanMae, string requestResult)
+        {
+            _time = time;
+            _meanScalePredict = meanScalePredict;
+            _meanScaleLoad = meanScaleLoad;
+            _meanMae = meanMae;
+            _requestResult = requestResult;
+        }
+        #endregion
+        #region ToString 재정의
+        public override string ToString() => $"Time : {_time}, MeanScalePredict : {_meanScalePredict}, MeanScaleLoad : {_meanScaleLoad}, MeanMae : {_meanMae}, RequestResult : {_requestResult}";
+        #endregion
+    }
+    public class HttpProductInformation
+    {
+        #region 속성
+        [JsonPropertyName("op")]
+        public string _opcode { get; set; }
+        [JsonPropertyName("sn")]
+        public string _serialNumber { get; set; }
+        [JsonPropertyName("acc")]
+        public string _accuracy { get; set; }
+        [JsonPropertyName("predict")]
+        public string _predict { get; set; }
+        [JsonPropertyName("startTime")]
+        public string _startTime { get; set; }
+        [JsonPropertyName("endTime")]
+        public string _endTime { get; set; }
+        [JsonPropertyName("mae")]
+        public string _mae { get; set; }
+        public string _requestResult { get; set; }
+        #endregion
+        #region 생성자
+        public HttpProductInformation(){}
+        //임시 생성자
+        public HttpProductInformation(string opcode, string serialNumber, string accuracy, string predict, string startTime, string endTime, string requestResult)
+        {
+            _opcode = opcode;
+            _serialNumber = serialNumber;
+            _accuracy = accuracy;
+            _predict = predict;
+            _startTime = startTime;
+            _endTime = endTime;
+            _requestResult = requestResult;
+        }
+        public HttpProductInformation(string opcode, string serialNumber, string accuracy, string predict, string startTime, string endTime, string mae, string requestResult)
+        {
+            _opcode = opcode;
+            _serialNumber = serialNumber;
+            _accuracy = accuracy;
+            _predict = predict;
+            _startTime = startTime;
+            _endTime = endTime;
+            _mae = mae;
+            _requestResult = requestResult;
+        }
+        #endregion
+        #region ToString 재정의
+        public override string ToString() => $"OPCode : {_opcode},SerialNumber : {_serialNumber},Accuracy : {_accuracy},Predict : {_predict},StartTime : {_startTime},EndTime : {_endTime},Mae : {_mae}, RequestResult : {_requestResult}";
+        #endregion
+    }
+    public class HttpQualityInformaiton
+    {
+        #region 속성
+        [JsonPropertyName("op")]
+        public string _opcode { get; set; }
+        [JsonPropertyName("sn")]
+        public string _serialNumber { get; set; }
+        [JsonPropertyName("fn")]
+        public string _fileName { get; set; }
+        [JsonPropertyName("acc")]
+        public string _accuracy { get; set; }
+        [JsonPropertyName("predict")]
+        public string _predict { get; set; }
+        [JsonPropertyName("startTime")]
+        public string _startTime { get; set; }
+        [JsonPropertyName("endTime")]
+        public string _endTime { get; set; }
+        [JsonPropertyName("predictImg")]
+        [JsonConverter(typeof(ByteArrayConverter))]
+        public Byte[] _imageBytes { get; set; }
+        [JsonPropertyName("imgSize")]
+        public int _imageSize { get; set; }
+        public string _requestResult { get; set; }
+        #endregion
+
+        #region 생성자
+        public HttpQualityInformaiton(){}
+
+        public HttpQualityInformaiton(string opcode, string serialNumber, string fileName, string accuracy, string predict, string startTime, string endTime, byte[] imageBytes, int imageSize, string requestResult)
+        {
+            _opcode = opcode;
+            _serialNumber = serialNumber;
+            _fileName = fileName;
+            _accuracy = accuracy;
+            _predict = predict;
+            _startTime = startTime;
+            _endTime = endTime;
+            _imageBytes = imageBytes;
+            _imageSize = imageSize;
+            _requestResult = requestResult;
+        }
+        #endregion
+        #region ToString 재정의
+        public override string ToString() => $"OPCode : {_opcode},SerialNumber : {_serialNumber},FileName : {_fileName},Accuracy : {_accuracy},Predict : {_predict}, ImageBytes : {_imageBytes}, ImageSize : {_imageSize}, RequestResult : {_requestResult}";
+        #endregion
+    }
+    public class HttpAuthentication
+    {
+        #region 속성
+        public bool _checkPassword { get; set; }
+        public string _processResult { get; set; }
+        #endregion
+        #region 생성자
+        public HttpAuthentication()
+        {
+        }
+        public HttpAuthentication(bool checkPassword, string processResult)
+        {
+            _checkPassword = checkPassword;
+            _processResult = processResult;
+        }
+        #endregion
+        #region ToString 재정의
+        public override string ToString() => $"CheckPassword : {_checkPassword}, ProcessResult : {_processResult}";
+        #endregion
+    }
+    //public class ErrorMessage
+    //{
+    //    #region 속성
+    //    public string _errorMessage { get; set; }
+    //    #endregion
+    //    #region 생성자 
+    //    [JsonConstructor]
+    //    public ErrorMessage(string errorMessage)
+    //    {
+    //        _errorMessage = errorMessage;
+    //    }
+    //    #endregion
+    //    #region ToString 재정의
+    //    public override string ToString() => $"ErrorMessage : {_errorMessage}";
+    //    #endregion
+    //}
+    public enum HttpOPCode 
+    {
+        OP10_3,
+    }
+    public enum HttpClassification
+    {
+        day,
+        week,
+        month,
+        custom,
+    }
+    public enum HttpAbnormalProblems
+    {
+        Normal,
+        T8080_Wear,
+        T2030_Wear,
+        T2020_Wear,
+        T6070_Wear,
+        T6060_Wear,
+        T1212_Wear,
+        T8080_Broken,
+        T2030_Broken,
+        T2020_Broken,
+        T6070_Broken,
+        T6060_Broken,
+        T1212_Broken,
+    }
+}
---
+++ HNInc.Communication.Library/Socket/HNSocketIO.cs
@@ -0,0 +1,316 @@
+﻿using SocketIOClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace HNInc.Communication.Library
+{
+    public class HNSocketIO
+    {
+        //SocektIO Library
+        private SocketIO _socketIO;
+        #region field
+        // 해당 회수만큼 Connect 시도
+        private readonly int _reconnectionLimmit = 5;
+        private int _reconnectionCount = 0;
+        #endregion 
+        #region Event
+        public delegate void FuncDelegate(string evetName, object data);
+        public delegate void DeliverDelegate(object sender, object data);
+        public event FuncDelegate AnyDataEvent;
+        public event FuncDelegate RealTimeLossEvent;
+        public event FuncDelegate RealTimeLoadEvent;
+        public event FuncDelegate QualityJudgmentProgresStartEvent;
+        public event FuncDelegate QualityJudgmentProgresEndEvent;
+        public event FuncDelegate ProductInformationEvent;
+        public event DeliverDelegate SocketConnectedEvent;
+        public event DeliverDelegate SocketDisConnectedEvent;
+        public event DeliverDelegate SocketConnectingEvent;
+        #endregion
+        //Default
+        #region Constructor
+        public HNSocketIO()
+        {
+            _socketIO = new SocketIO(new Uri("http://9.8.100.153:8082/"), new SocketIOOptions
+            {
+                Query = new Dictionary<string, string>
+                {
+                    {"token", "V4" }
+                },
+                EIO = 4,
+                Transport = SocketIOClient.Transport.TransportProtocol.WebSocket
+            });
+            _socketIO.OnConnected += SocketOnConnected;
+            _socketIO.OnPing += SocketOnPing;
+            _socketIO.OnPong += SocketOnPong;
+            _socketIO.OnDisconnected += SocketOnDisconnected;
+            _socketIO.OnReconnectAttempt += SocketOnReconnecting;
+        }
+        public HNSocketIO(Uri uri, SocketIOClient.Transport.TransportProtocol transport)
+        {
+            _socketIO = new SocketIO(uri, new SocketIOOptions
+            {
+                Query = new Dictionary<string, string>
+                {
+                    {"token", "V4" }
+                },
+                EIO = 4,
+                Transport = transport
+            });
+            _socketIO.OnConnected += SocketOnConnected;
+            _socketIO.OnPing += SocketOnPing;
+            _socketIO.OnPong += SocketOnPong;
+            _socketIO.OnDisconnected += SocketOnDisconnected;
+            _socketIO.OnReconnectAttempt += SocketOnReconnecting;
+        }
+        public HNSocketIO(Uri uri, SocketIOClient.Transport.TransportProtocol transport, TimeSpan connectionTimeout, int reconnectionAttempts)
+        {
+            _socketIO = new SocketIO(uri, new SocketIOOptions
+            {
+                Query = new Dictionary<string, string>
+                {
+                    {"token", "V4" }
+                },
+                EIO = 4,
+                Transport = transport,
+                ConnectionTimeout = connectionTimeout,
+                ReconnectionAttempts = reconnectionAttempts
+            });
+            _socketIO.OnConnected += SocketOnConnected;
+            _socketIO.OnPing += SocketOnPing;
+            _socketIO.OnPong += SocketOnPong;
+            _socketIO.OnDisconnected += SocketOnDisconnected;
+            _socketIO.OnReconnectAttempt += SocketOnReconnecting;
+        }
+        #endregion 
+        #region SocketIO Event
+        private void SocketOnReconnecting(object sender, int e)
+        {
+            Console.WriteLine($"{DateTime.Now} Reconnecting: attempt = {e}");
+            if (SocketConnectingEvent != null)
+            {
+                SocketConnectingEvent(sender, e);
+            }
+            else
+            {
+                Debug.WriteLine("SocketConnectingEvent is NULL, Register SocketConnectingEvent");
+            }
+        }
+        private void SocketOnDisconnected(object sender, string e)
+        {
+            Console.WriteLine("disconnect: " + e);
+            if (SocketDisConnectedEvent != null)
+            {
+                SocketDisConnectedEvent(sender, e);
+            }
+            else
+            {
+                Debug.WriteLine("SocketDisConnectedEvent is NULL, Register SocketDisConnectedEvent");
+            }
+        }
+        private void SocketOnConnected(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Socket_OnConnected");
+            var socket = sender as SocketIO;
+            Debug.WriteLine("Socket.Id:" + socket.Id);
+            if (SocketConnectedEvent != null)
+            {
+                SocketConnectedEvent(sender, e);
+            }
+            else
+            {
+                Debug.WriteLine("SocketOnConnected is NULL, Register SocketOnConnected");
+            }
+        }
+        private static void SocketOnPing(object sender, EventArgs e)
+        {
+            Console.WriteLine("Ping");
+        }
+        private static void SocketOnPong(object sender, TimeSpan e)
+        {
+            Console.WriteLine("Pong: " + e.TotalMilliseconds);
+        }
+        #endregion 
+        #region Method
+        public void Connect()
+        {
+            _socketIO.ConnectAsync();
+        }
+        public Boolean IsConnected()
+        {
+            return _socketIO.Connected;
+        }
+        public void Disconnect()
+        {
+            _socketIO.DisconnectAsync();
+            _socketIO.Dispose();
+        }
+        public void SendData(SocketEventNames eventName)
+        {
+            try
+            {
+                _socketIO.EmitAsync(eventName.ToString(), DateTime.Now.ToString());
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"{DateTime.Now} Error Message : {e.ToString()}");
+            }
+        }
+        public void ReceiveAnyData()
+        {
+            _socketIO.OnAny((eventName, response) =>
+            {
+                if (AnyDataEvent != null)
+                {
+                    AnyDataEvent(eventName, response.GetValue().ToString());
+                }
+                else
+                {
+                    Debug.WriteLine("AnyDataEvent is NULL, Register AnyDataEvent");
+                }
+            });
+        }
+        public void OffReceiveAnyData()
+        {
+            //_socketIO.OffAny(handler);
+        }
+        public void ReceiveRealTimeLossData()
+        {
+            string eventName = SocketEventNames.RTLossData.ToString();
+            _socketIO.On(eventName, response =>
+            {
+                var responseValue = response.GetValue();
+                string mae = responseValue.GetProperty("mae").GetRawText();
+                string anomaly = responseValue.GetProperty("anomaly").GetString();
+                SocketRealTimeLoss realTimeLoss = new SocketRealTimeLoss(mae, anomaly);
+                if (RealTimeLossEvent != null)
+                {
+                    RealTimeLossEvent(eventName, realTimeLoss);
+                }
+                else
+                {
+                    Debug.WriteLine("RealTimeLossEvent is NULL, Register RealTimeLossEvent");
+                }
+            });
+        }
+        public void ReceiveRealTimeLoadData()
+        {
+            string eventName = SocketEventNames.RTLoadData.ToString();
+            _socketIO.On(eventName, response =>
+            {
+
+                //split try catch 설정
+                string[] receiveDataArray;
+                try
+                {
+                    receiveDataArray = response.ToString().Split(new string[] { "[", "]", "\"", ",", "\\n" }, StringSplitOptions.RemoveEmptyEntries);
+                }
+                catch (Exception e)
+                {
+                    receiveDataArray = null;
+                    Debug.WriteLine("Fail to Split RealTimeLoad, Error Message: {0}, Error Target: {1}", e.Message, e.TargetSite);
+                }
+                List<SocketRealTimeLoad> rTLoads = new List<SocketRealTimeLoad>();
+                SocketRealTimeLoad realTimeLoad;
+                // 배열 null 혹은 개수 체크
+                if (receiveDataArray != null)
+                {
+                    for (int i = 0; i < receiveDataArray.Length; i = i + 9)
+                    {
+                        realTimeLoad = new SocketRealTimeLoad(receiveDataArray[i], receiveDataArray[i + 1], receiveDataArray[i + 2], receiveDataArray[i + 3],
+                            receiveDataArray[i + 4], receiveDataArray[i + 5], receiveDataArray[i + 6], receiveDataArray[i + 7], receiveDataArray[i + 8]);
+                        rTLoads.Add(realTimeLoad);
+                    }
+                }
+                if (RealTimeLoadEvent != null)
+                {
+                    RealTimeLoadEvent(eventName, rTLoads);
+                }
+                else
+                {
+                    Debug.WriteLine("RealTimeLoadEvent is NULL, Register RealTimeLoadEvent");
+                }
+            });
+        }
+        public void ReceiveQualityJudgmentProgresStart()
+        {
+            string eventName = SocketEventNames.judgeQualityStart.ToString();
+            _socketIO.On(eventName, response =>
+            {
+                var responseValue = response.GetValue();
+                string opCode = responseValue.GetProperty("op").GetString();
+                string serialNumber = responseValue.GetProperty("sn").GetString();
+                SocketQualityInformation qualityInformation = new SocketQualityInformation(opCode, serialNumber);
+                if (QualityJudgmentProgresStartEvent != null)
+                {
+                    QualityJudgmentProgresStartEvent(eventName, qualityInformation);
+                }
+                else
+                {
+                    Debug.WriteLine("QualityJudgmentProgresStartEvent is NULL, Register QualityJudgmentProgresStartEvent");
+                }
+            });
+        }
+        public void ReceiveQualityJudgmentProgressEnd()
+        {
+            string eventName = SocketEventNames.judgeQualityEnd.ToString();
+            _socketIO.On(eventName, response =>
+            {
+                var responseValue = response.GetValue();
+                string opCode = responseValue.GetProperty("op").GetString();
+                string serialNumber = responseValue.GetProperty("sn").GetString();
+                string accuracy = responseValue.GetProperty("acc").GetString();
+                string temp = responseValue.GetProperty("predict").GetString();
+                string predict = Enum.GetName(typeof(SocketAbnormalProblems), Int32.Parse(temp));
+                string fileName = responseValue.GetProperty("fn").GetString();
+                Byte[] imageBytes = response.InComingBytes[0];
+
+                SocketQualityInformation qualityInformation = new SocketQualityInformation(opCode, serialNumber, accuracy, predict, fileName, imageBytes);
+                if (QualityJudgmentProgresEndEvent != null)
+                {
+                    QualityJudgmentProgresEndEvent(eventName, qualityInformation);
+                }
+                else
+                {
+                    Debug.WriteLine("QualityJudgmentProgresEndEvent is NULL, Register QualityJudgmentProgresEndEvent");
+                }
+            });
+        }
+        public void ReceiveProductInformation()
+        {
+            string eventName = SocketEventNames.productInformation.ToString();
+            _socketIO.On(eventName, response =>
+            {
+                var responseValue = response.GetValue();
+                string opCode = responseValue.GetProperty("op").GetString();
+                string serialNumber = responseValue.GetProperty("sn").GetString();
+                string accuracy = responseValue.GetProperty("acc").GetString();
+                string temp = responseValue.GetProperty("predict").GetString();
+                string predict = Enum.GetName(typeof(SocketAbnormalProblems), Int32.Parse(temp));
+                string startTime = responseValue.GetProperty("startTime").GetString();
+                string endTime = responseValue.GetProperty("endTime").GetString();
+                string mae = responseValue.GetProperty("mae").GetString();
+                if (temp.Equals("0") && mae.Equals("abnormal"))
+                {
+                    predict = "Abnormal data exists. Please check...";
+                }
+                SocketProductInformation productInformation = new SocketProductInformation(opCode, serialNumber, accuracy, predict, startTime, endTime, mae);
+                if (ProductInformationEvent != null)
+                {
+                    ProductInformationEvent(eventName, productInformation);
+                }
+                else
+                {
+                    Debug.WriteLine("ProductInformationEvent is NULL, Register ProductInformationEvent");
+                }
+            });
+        }
+        public void OffReceiveData(SocketEventNames eventName)
+        {
+            string eventNameString = eventName.ToString();
+            _socketIO.Off(eventNameString);
+        }
+        #endregion 
+    }
+}
+
